092ee05e8712cd2182d5190e9c85e83f3c83a245
aj223bm <aj223bm@studentl.lnu.se>
2016-12-20 11:36:32 +0100

    Book and Author class

--- src/Book.java
+++ src/Book.java
@@ -3,5 +3,58 @@
  */
 public class Book {
 
-    private String
+    private String title;
+    private String author;
+    private int year;
+    private int edition;
+    private String category;
+    private String subCategory;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+
+    public void setEdition(int edition) {
+        this.edition = edition;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
+    }
 }
