cba783d2387e2a5f919c639c299e46c460727af9
mohamed osman <my222au@student.lnu.se>
2016-12-21 14:53:50 +0100

    add  some code to view

--- src/User.java
+++ src/User.java
@@ -1,15 +1,47 @@
+import java.sql.DatabaseMetaData;
+import java.util.ArrayList;
+
 /**
  * Created  bty mohamed  on 2016-12-21.
  */
 public class User {
     View view ;
+    DatabaseHelper db;
+    Book  book;
+    Author author;
+    ArrayList<Book> books = new ArrayList<Book>();
     public User(View view) {
         this.view = view;
+        book = new Book();
+        author  = new Author();
+
+        system();
+
+
+    }
+    private void  system(){
+      while (true) {
+          view.showMenu();
+          if(view.getInput() == 1){
+              addbook();
+          }else{
+              break;
+          }
+
+
+
+      }
+
+
+
+
     }
 
-    // Show Welcome Message
-    private
-    // add books
+    private void addbook() {
+        view.getBookInformation(book,author);
+        System.out.println(book.getAuthor());
+
+    }
 
     // serach books
     // remove books
--- src/View.java
+++ src/View.java
@@ -1,10 +1,55 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by mohamed on 2016-12-21.
  */
 public class View {
+
+    Scanner scanner;
+
     //  view welcome message
     // view menus
-    //  respond to User events
+    public void showWelcomeMessage() {
+
+        System.out.println(" Welcome to Your Personal Library");
+
+    }
+
+    public void showMenu() {
+
+        System.out.println("1.  Add new book ");
+
+    }
+
+    public int getInput() {
+        scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public void getBookInformation(Book book, Author author) {
+        scanner = new Scanner(System.in);
+        String input = " ";
+        System.out.println(" Title of the Books ");
+        while (scanner.hasNext()) {
+            input = scanner.nextLine();
+            book.setTitle(input);
+            if (scanner.next().equals('\n')) {
+                System.out.println("Name of the author");
+                input = scanner.nextLine();
+                author.setName(input);
+                book.setAuthor(author.getName());
+            }
+
+        }
+        scanner.close();
+
+    }
 
 
 }
+
+
+
+
+
