9e1e170676b7b953037fb6e8b08e1430c075628d
mohamed osman <my222au@student.lnu.se>
2016-12-21 13:56:36 +0100

    added three classes User, View and main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
-import java.sql.*;
-
 /**
- * Created by adamjohansson on 2016-12-20.
+ * Created by Armend on 2016-12-21.
  */
 public class Main {
 
-
-
-
-    public static void main(String args[]) {
-
-
-
-        addBooks();
+    public static void main(String[] args) {
+        View view = new View();
+        User user = new User(view);
     }
-
-    private static void addBooks() {
-
-    }
-
-
 }
---
+++ src/View.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mohamed on 2016-12-21.
+ */
+public class View {
+    //  view welcome message
+    // view menus
+    //  respond to User events
+
+
+}
